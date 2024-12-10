38a72aa380e9ca36c8193a5daef93eee0b3a10e5
NarsiBhatii <narsibhati31102@gmail.com>
2024-12-10 22:00:29 +0530

    struct in go

--- GoDay4/12_struct/struct.go
+++ GoDay4/12_struct/struct.go
@@ -1,33 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-// type order struct {
-// 	id     string
-// 	amount float32
-// 	status string
-// 	createdAt time.Time //
-// }
+type order struct {
+	id     string
+	amount float32
+	status string
+	createdAt time.Time // time package
+}
 
-// func newOrder(id string,amount float32,status string) *order {
-// 	// initial setup goes here...
-// 	myOrder := order  {
-// 		id: id,
-// 		amount: amount,
-// 		status: status,
-// 		createdAt: time.Now(),
-// 	}
-// 	return &myOrder
-// }
+func newOrder(id string,amount float32,status string) *order {
+	// initial setup goes here...
+	myOrder := order  {
+		id: id,
+		amount: amount,
+		status: status,
+		createdAt: time.Now(),
+	}
+	return &myOrder
+}
 
-// // receiver type
-// func (o *order) changeStatus(status string) {
-// 	o.status = status
-// }
+// receiver type
+func (o *order) changeStatus(status string) {
+	o.status = status
+}
 
-// func (o order) getAmount() float32 {
-// 	return o.amount
-// }
+func (o order) getAmount() float32 {
+	return o.amount
+}
 
 
 func main() {
@@ -39,32 +42,32 @@ func main() {
 	fmt.Println(language)
 
 
-	// myOrder := order  {
-	// 	id: "1",
-	// 	amount: 50.00,
-	// 	status: "received",
-	// 	createdAt: time.Now(),
-	// }
+	myOrder := order  {
+		id: "1",
+		amount: 50.00,
+		status: "received",
+		createdAt: time.Now(),
+	}
 
 	// myOrder := newOrder("1",30.50,"received")
 
-	// fmt.Println(myOrder)
+	fmt.Println(myOrder)
 
-	// myOrder.changeStatus("confirmed")
-	// fmt.Println(myOrder.getAmount())
+	myOrder.changeStatus("confirmed")
+	fmt.Println(myOrder.getAmount())
 
-	// myOrder.createdAt = time.Now()
+	myOrder.createdAt = time.Now()
 
-	// fmt.Println("Order Status :",myOrder.status)
+	fmt.Println("Order Status :",myOrder.status)
 
-	// fmt.Println("order struct",myOrder)
+	fmt.Println("order struct",myOrder)
 
-	// myOrder2 := order {
-	// 	id: "2",
-	// 	amount: 60.00,
-	// 	status: "pending",
-	// 	createdAt: time.Now(),
-	// }
+	myOrder2 := order {
+		id: "2",
+		amount: 60.00,
+		status: "pending",
+		createdAt: time.Now(),
+	}
 
-	// fmt.Println("order 2 struct",myOrder2)
+	fmt.Println("order 2 struct",myOrder2)
 }
